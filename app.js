452944c07311081a869d65bd125a7f41e21662a1
M Yaseen <yaseenghouri2@gmail.com>
2024-02-29 17:29:22 +0500

    Assign 18

---
+++ app.js
@@ -0,0 +1,21 @@
+// Chapter 9-11
+// // Q1
+// var city = prompt("Enter the name of a city:");
+// if (city === "Karachi"){
+//     alert ("Welcome to the city of lights")}
+// else if(city !== "Karachi"){
+//     alert("Welcome")}
+
+// // Q2
+// var gender = prompt("Enter your gender:");
+// if (gender === "Male"){
+//     alert ("Good Morning Sir")}
+// else if(gender !== "Male"){
+//     alert("Good Morning Ma'am")}
+
+// Q3
+// document.write("<table><tr><td>Signal color</td><td>Message</td></tr></table>");
+var red = prompt("Enter a traffic signal color");
+var yellow = prompt("Enter a traffic signal color");
+var green = prompt("Enter a traffic signal color");
+document.write("<table><tr><td>Signals</td></tr>" + "<tr><td>Red</td><td>"+red+"</td></tr>" +"<tr><td>
